f009e28945a8342143adeaa18bce49a3fdba7c53
Neshyna <mariya.neshyna@gmail.com>
2025-01-22 00:51:59 +0100

    hw4 neshyna

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,46 @@
+import "./styles.css";
+import Button from "../Button/Button";
+
+import { useState } from "react";
+
+function Feedback() {
+  const [numberOfLikes, addLike] = useState(0);
+const [numberOfDislikes, addDislike] = useState(0);
+
+  const clickLike = () => {
+    addLike((prevState) => prevState + 1);
+  };
+
+  const clickDislike =()=>{
+    addDislike((prevState) => prevState + 1);
+  };
+
+  const resetResults=()=>{
+    addLike(0);
+    addDislike(0);
+  }
+
+  return (
+    <div className="feedback-wrapper">
+      <div className="like-wrapper">
+      <div className="number-likes">{numberOfLikes}</div>
+      <div className="button-wrapper">
+        <Button name="Like" type="button" onClick={clickLike} />
+      </div>
+      </div>
+      <div className="dislike-wrapper">
+         <div className="button-wrapper">
+        <Button name="Dislike" type="button" onClick={clickDislike} />
+      </div>
+      <div className="number-dislike">{numberOfDislikes}</div>
+      </div>
+      <div className="reset-wrapper">
+      <div className="button-wrapper">
+      <Button name="Reset Results" type="button" onClick={resetResults} />
+      </div>
+      </div>
+
+    </div>
+  );
+}
+export default Feedback;
---
+++ src/hw/hw4/HW4.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Feedback from "../../components/Feedback/Feedback";
+
+function HW4() {
+  return (
+    <div className="hw4-wrapper">
+      <Feedback />
+    </div>
+  );
+}
+export default HW4;
