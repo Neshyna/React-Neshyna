912dadfb454a655552df0526ddb8d795083489da
Neshyna <mariya.neshyna@gmail.com>
2025-01-23 16:04:26 +0100

    add

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -10,3 +10,21 @@ function showProduct({productName,productPrice}) {
 }
 
 export default showProduct;
+
+
+
+import "./styles.css";
+
+// function Product({ productName = "Unknown", productPrice }) {
+//   return (
+//     <div className="product-wrapper">
+//       <h3>{productName}</h3>
+//       {/* Пример тернарного оператора для скрытия элемента div, если цена не была передана */}
+//       {/* {productPrice !== undefined ? <div>{productPrice} $</div> : null} */}
+//       {/* Пример использования оператора условного рендеринга - && */}
+//       {productPrice !== undefined && <div>{productPrice} $</div>}
+//     </div>
+//   );
+// }
+
+// export default Product;
